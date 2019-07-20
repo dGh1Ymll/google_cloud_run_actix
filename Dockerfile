ca24a7cff18fbe74591c13dac8c694a076582d64
dGh1Ymll <>
2019-07-20 20:51:17 +0200

    Initial code setup for cloud run

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
-FROM gcr.io/actix-test/cargo:latest
+FROM gcr.io/actix-test/cargo:latest as builder
 
 workdir /usr/src/project
 
 COPY . .
 
-RUN cargo build --release
+RUN cargo build --target x86_64-unknown-linux-musl --release
 
+FROM alpine:latest
+
+COPY --from=builder /target/x84_64-unknown-linux-musl/release/google_cloud_run_actix
+
+CMD["/google_cloud_run_actix"]
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
 fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
 fn main() {
+    
+    let mut host = "127.0.0.1:".to_string();
+
+
+    match env::var("PORT") {
+        Ok(port) => {
+            host = host + &port.to_string();
+            println!("Localhost and port: {}", host);
+        }
+        Err(e) => println!("Couldn't read PORT ({})", e),
+    };
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
     })
-    .bind("127.0.0.1:8088")
+    .bind(host)
     .unwrap()
     .run()
     .unwrap();
